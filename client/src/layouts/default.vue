a206028d56b02df6151b41ccfe24247f7e8196e0
José Israel <hswolf@proton.me>
2024-04-08 23:49:13 -0300

    Updated MainNavbar to be responsive.
    Added mock pages to test MainNavbar.

--- client/src/layouts/default.vue
+++ client/src/layouts/default.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="relative flex flex-col md:flex-row h-full pt-12 md:pt-0 md:ps-12">
+  <div
+    class="relative flex flex-col md:flex-row h-full pt-12 md:pt-0 md:ps-12"
+  >
     <MainNavbar />
     <slot />
   </div>
